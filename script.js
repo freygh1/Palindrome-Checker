cdcb5855429d0026168a9b7868c92d8c29f6fcba
freygh1 <freilinshernandez10@gmail.com>
2025-07-16 16:55:04 -0400

    I don't like to make commitments, sorry

---
+++ script.js
@@ -0,0 +1,55 @@
+const textInput = document.querySelector('#text-input');
+const checkBtn = document.querySelector('#check-btn');
+const result = document.querySelector('#result')
+
+const modalBox = document.getElementById('description-box')
+const modalContent = document.getElementById('description-content')
+const showDescriptionBtn = document.getElementById('show_description')
+const closeModalBtn = document.getElementById('close')
+
+const isAPalindrome = "is a palindrome"
+const isNotAPalindrome = "is not a palindrome"
+
+const collections = [
+  "Put some words, man!",
+  "Do you want to check for something or not?",
+  "Just do it, bro",
+  "I think you have a lot of free time, dude.",
+  "Play some music for me, please. I'm boring.",
+  "Don't waste my time, Amigo.",
+  "Do you wanna test me, right?",
+  "Do you really have nothing better to do?",
+  "Pack it in, friend."
+]
+
+checkBtn.addEventListener('click', () => {
+  if (textInput.value === '') {
+    const randomPhrase = Math.floor(Math.random() * collections.length)
+    result.textContent = collections[randomPhrase]
+  } else {
+    const myInput = textInput.value.replace(/[^A-Za-z0-9]/gi, '').toLowerCase()
+
+    if (myInput === [...myInput].reverse().join('')) {
+      result.textContent = `${textInput.value} ${isAPalindrome}`
+    } else {
+      result.textContent = `${textInput.value} ${isNotAPalindrome}`
+    }
+
+    textInput.value = '';
+    textInput.focus()
+  }
+
+
+})
+
+showDescriptionBtn.addEventListener('click', () => {
+  modalBox.classList.remove('hidden')
+  modalBox.classList.add('flex')
+})
+
+closeModalBtn.addEventListener('click', () => {
+  modalBox.classList.remove('flex')
+  modalBox.classList.add('hidden')
+})
+
+
